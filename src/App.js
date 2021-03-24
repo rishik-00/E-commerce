d240546fce0d40a25b7b9611022425bcf3255f47
Rishik <rishikgoyal00@gmail.com>
2021-03-24 18:22:45 +0530

    sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 import './App.css';
 
@@ -10,9 +12,11 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+         <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
